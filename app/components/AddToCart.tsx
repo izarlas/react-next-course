0c7bf434a65c7afc0c7fcf2ce056d2ea0a778b0b
Ioannis Zarlas <ioannis.zarlas@chromaway.com>
2025-06-25 15:35:23 +0300

    Server side components with mock api. Users Page added

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button onClick={() => console.log("I was clicked")}>Add to cart</button>
+    </div>
+  );
+};
+
+export default AddToCart;
