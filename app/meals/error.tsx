5d7084340f94fc37d477d35f884a2da46a4d17a3
Ioannis Zarlas <ioannis.zarlas@chromaway.com>
2025-06-30 20:21:17 +0300

    Fixed typo

--- app/meals/error.tsx
+++ app/meals/error.tsx
@@ -3,7 +3,7 @@
 export default function Error({ error }: { error: Error }) {
   return (
     <div className="text-2xl flex flex-col">
-      <div>An error occured!</div>
+      <div>An error occurred!</div>
       <div>Error: {error.message}</div>
     </div>
   );
