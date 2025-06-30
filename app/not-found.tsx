8862e09dc55e2162a02201dab91e730a4ab3fc07
Ioannis Zarlas <ioannis.zarlas@chromaway.com>
2025-06-30 20:20:23 +0300

    Added not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="text-2xl flex flex-col">
+      <div>Not found!</div>
+      <div>The requested page was not found!</div>
+    </div>
+  );
+}
